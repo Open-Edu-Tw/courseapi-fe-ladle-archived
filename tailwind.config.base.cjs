938765533db6b56658f9a2308cc84eb78a2373e5
pan93412 <pan93412@gmail.com>
2022-06-16 03:18:11 +0000

    refactor!(tailwind): remove deprecated overrrides

--- tailwind.config.base.cjs
+++ tailwind.config.base.cjs
@@ -2,18 +2,7 @@
 module.exports = {
 	content: [],
 	theme: {
-		extend: {
-			colors: {
-				primary: '#1F1F1F',
-				secondary: '#F2F2F2',
-				background: '#FAFAFA',
-				accent: '#1DA7CC',
-				'accent-background': '#BDD8E0',
-			},
-		},
-		borderRadius: {
-			DEFAULT: '0.7rem',
-		},
+		extend: {},
 	},
 	plugins: [],
 };
